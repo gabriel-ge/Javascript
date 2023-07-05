7b9986bb802b004e3ca4a413c6f27afbb922c166
Gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-05 14:48:26 -0300

    Enviando variáveis, let e const

---
+++ operador-ternario.js
@@ -0,0 +1,16 @@
+
+const idadeMinima = 18;
+const idadeCliente = 18;
+
+
+// ------- IF ------- //
+
+if (idadeCliente >= idadeMinima) {
+    console.log("Cerveja");
+} else {
+    console.log("Suco");
+}
+
+// ------ OPERADOR TERNARIO ------ //
+
+console.log(idadeCliente >= idadeMinima ? "Cerveja" : "Suco");
---
+++ templates.js
@@ -0,0 +1,7 @@
+const nome = "Gabriel";
+const idade = 2023 - 2000;
+const cidadeNascimento = "São Leopoldo";
+
+const apresentacao = `Meu nome é ${nome}, tenho ${idade} anos e nasci na cidade de ${cidadeNascimento}.`;
+
+console.log(apresentacao);
