743b7418c87ad26acdfff04e28e736d5f72a3627
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-07 15:37:48 -0300

    Arquivos de estudo atualizados

---
+++ Javascript-Arrays/Javascript-slice.js
@@ -0,0 +1,14 @@
+const pessoas = [
+    "Gabriel",
+    "Pedro",
+    "Lucas",
+    "Rodrigo",
+    "allan",
+    "guilherme"
+];
+
+const pessoas1 = pessoas.slice(0, pessoas.length / 2);
+const pessoas2 = pessoas.slice(pessoas.length / 2)
+
+console.log(pessoas1);
+console.log(pessoas2);
