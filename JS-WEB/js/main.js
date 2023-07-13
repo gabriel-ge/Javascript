75336820a24f1169d46d517521aea8876a7a440f
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-12 23:46:35 -0300

    Update código: Adicinando novos itens e armazenando informações no LocalStorage do navegador

--- JS-WEB/js/main.js
+++ JS-WEB/js/main.js
@@ -1,11 +1,18 @@
 const form = document.getElementById("novoItem")
 const lista = document.getElementById("lista")
+const items = []
 
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    criaElemento(evento.target.elements["nome"].value, evento.target.elements["quantidade"].value)
 
+    const nome = evento.target.elements["nome"]
+    const quantidade = evento.target.elements["quantidade"]
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
 })
 
 function criaElemento(nome, quantidade) {
@@ -19,4 +26,13 @@ function criaElemento(nome, quantidade) {
     novoItem.innerHTML += nome
 
     lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    items.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(items))
 }
