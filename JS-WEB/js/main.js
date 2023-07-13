b4d90561bac40eb59dd6714e0999652c8bd975e3
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-13 12:06:18 -0300

    Update final: Finalizando código do curso JS-WEB. Funções de adicionar e remover itens

--- JS-WEB/js/main.js
+++ JS-WEB/js/main.js
@@ -23,10 +23,9 @@ form.addEventListener("submit", (evento) => {
         itemAtual.id = existe.id
 
         atualizaElemento(itemAtual)
-
-        itens[existe.id] = itemAtual
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     } else {
-        itemAtual.id = itens.length
+        itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
 
         criaElemento(itemAtual)
 
@@ -50,9 +49,33 @@ function criaElemento(item) {
 
     novoItem.innerHTML += item.nome
 
+    novoItem.appendChild(botaoDeleta(item.id)) // Referenciar a função botaoDeleta no nó da função principal
+
     lista.appendChild(novoItem)
 }
 
 function atualizaElemento(item) {
     document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
+}
+
+//Função para criar botão com evento de click nos itens, e retornar os itens clicados
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button")
+    elementoBotao.innerText = "X"
+
+    elementoBotao.addEventListener("click", function () {
+        deletaElemento(this.parentNode, id)
+    })
+
+    return elementoBotao
+}
+
+//Função para deletar os itens enviados da função botaoDeleta no array de itens e no navegador
+
+function deletaElemento(tag, id) {
+    tag.remove()
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
 }
