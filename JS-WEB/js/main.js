342798a9359835f3ab1df58f1824fb89005e8706
gabriel-ge <gdr.gabrielramos@gmail.com>
2023-07-13 10:21:05 -0300

    Iniciando parte 4-5 do curso Javascript-WEB

--- JS-WEB/js/main.js
+++ JS-WEB/js/main.js
@@ -3,7 +3,7 @@ const lista = document.getElementById("lista")
 const itens = JSON.parse(localStorage.getItem("itens")) || []
 
 itens.forEach((elemento) => {
-    criaElemento(elemento);
+    criaElemento(elemento)
 })
 
 form.addEventListener("submit", (evento) => {
@@ -12,14 +12,26 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
     }
 
-    criaElemento(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
 
-    itens.push(itemAtual)
+        atualizaElemento(itemAtual)
+
+        itens[existe.id] = itemAtual
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -27,17 +39,20 @@ form.addEventListener("submit", (evento) => {
     quantidade.value = ""
 })
 
-
 function criaElemento(item) {
     const novoItem = document.createElement("li")
     novoItem.classList.add("item")
 
     const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
 
+function atualizaElemento(item) {
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
 }
